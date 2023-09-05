39773fff4bb1d9b84e758b4133ef1976b03c535e
Mauryn24 <johndoe@example.com>
2023-09-05 21:35:27 +0300

    arrays and loops

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/bash/node
+
+//create a string of arrays
+const  strings = ["C is fun", "Python is cool", "JavaScript is amazing"];
+//initialize a variable i
+let i = 0;
+//check if i is less than the length of the array
+while (i < strings.length) {
+    //print the string
+    console.log(strings[i]);
+    //increment
+    i++;
+}
