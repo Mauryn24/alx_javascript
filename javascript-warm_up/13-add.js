79f8682ab54ca2ee4202fbcb4d786817b0b34e3f
Mauryn24 <johndoe@example.com>
2023-09-05 21:56:49 +0300

    returns addition of two numbers

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,5 +1,10 @@
-//add function is called and is visible from outside
-function add(num1, num2) {
-    const sum = num1 + num2;
-    return sum;
-}
+#!/usr/bin/bash/node
+
+/*
+    exports is an object in Node.js that allows you to export variables, functions, or objects to be used in other modules or scripts.
+
+    add is the name of the function being exported.
+
+    (a, b) => a + b is an arrow function. It takes two parameters a and b, and it returns the result of adding a and b together. In other words, it's a concise way of writing a function that adds two numbers.
+*/
+exports.add = (a, b) => a + b;
