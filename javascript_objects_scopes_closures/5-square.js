048e54a3b104fc78aed6dc106b87c3fe19b79e34
Mauryn24 <johndoe@example.com>
2023-09-06 21:26:43 +0300

    square inherits from rectangle

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -5,7 +5,7 @@
 const Rectangle = require('./4-rectangle.js');
 
 class Square extends Rectangle {
-    constructor(size) {
+    constructor (size) {
         //call the constructor of the rectangle class using super
         super(size, size);
     }
