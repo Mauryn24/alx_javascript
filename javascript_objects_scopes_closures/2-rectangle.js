41248800ff0b05538b74b7e24a4125e73c53d3f1
Mauryn24 <johndoe@example.com>
2023-09-06 20:55:50 +0300

    width and haight

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(width, height) {
+    constructor(w, h) {
         if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
             //if w or h is equal to 0 or not a number
+            return {};
         } else {
             //initialize the attribute width with the value of w
             this.width = w;
