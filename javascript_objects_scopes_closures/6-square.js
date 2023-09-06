9bfb46ff9dcb2aecb1be66b6fcd236be5aa88840
Mauryn24 <johndoe@example.com>
2023-09-06 22:04:49 +0300

    square inherits from rectangle

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,4 +1,4 @@
-!/usr/bin/node
+#!/usr/bin/node
 
 /*
     Square class that inherits from rectangle
@@ -8,14 +8,19 @@ const PrevSquare = require('./5-square');
 
 class Square extends PrevSquare {
     charPrint() {
+        // Define a variable to hold the character to be printed
         const myChar = c === undefined ? 'X' : c;
+        // iterate through the height
         for (let i = 0; i < this.height; i++) {
+            // Define a variable to hold the row of the character
             let myVar = '';
-            let y = 0;
-            while (y < this.width) {
+            // iterate over the width
+            for (let j = 0; j < this.width; j++) {
+                //concentate the character to the row
                 myVar += myChar;
-                y++;
             }
+
+            // log the row of characters to the console
             console.log(myVar);
         }
     }
