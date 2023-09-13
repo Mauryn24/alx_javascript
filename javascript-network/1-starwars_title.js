b23d7b3c0d5b2e84a682c995cadd8c94a9a81577
Mauryn24 <johndoe@example.com>
2023-09-13 08:33:55 +0300

    prints the title of a Star Wars movie where the episode number matches a given integer.

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -2,21 +2,21 @@
 // this line specifies the interpreter to use Node.js
 
 const req = require('request');
-//import the request module
+// import the request module
 
 const id = process.argv[2];
-//get the url from the command line arguements
+// get the movie ID from the command line arguments
 
-const url = "https://swapi-api.alx-tools.com/api/films/${id}";
-//line constructs the URL by appending the extracted ID to the base URL
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+// construct the URL by appending the extracted ID to the base URL
 
-req.get(url, {encoding: "utf-8"})
-//This line sends an http GET request to the specified url with UTF-8 encoding
+req.get(url, { encoding: 'utf-8' })
+// This line sends an HTTP GET request to the specified URL with UTF-8 encoding
 
-.on('data', function(data) {
-    //event listener
-    const title = JSON.parse(data);
-    // turning json string to an object
-    console.log(movie.title);
+.on('data', function (data) {
+    // event listener
+    const title = JSON.parse(data).title;
+    // parse the JSON string to an object
+    console.log(title);
     // print the title
-})
+});
