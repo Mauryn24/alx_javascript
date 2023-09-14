9bf5024154ee23ec33dca9bac3443862fec4f544
Mauryn24 <johndoe@example.com>
2023-09-14 12:19:29 +0300

    computes the number of tasks completed by user id.

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,40 +1,40 @@
-#!/usr/bin/node
-// specifies the interpreter for the script
+// Import the request module
+const request = require('request');
 
-const req = require('request');
-// Import the 'request' module
+// Define the API URL
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
-if (process.argv.length < 4) {
-    console.error('Usage: node 3-request_store.js <URL> <File Path>');
-    process.exit(1); // Exit the script with an error
-}
-
-// The url to fetch
-const apiUrl = process.argv[2];
-
-// Initialize an object to store the count of completed tasks per user
-const completedTasksByUser = {};
-
-// Send an HTTP GET request to the specified API URL
-request(apiUrl, { json: true }, (error, response, data) => {
+// Make a GET request to the API
+request(API_URL, function(error, response, body) {
   if (error) {
-    console.error('Error:', error.message);
-    process.exit(1); // Exit the script with an error code in case of an error
+    console.log("Something went wrong while making the request to the API");
+    return;
   }
 
-  if (response.statusCode !== 200) {
-    console.error('Error: Unable to fetch data. Status code:', response.statusCode);
-    process.exit(1); // Exit the script with an error code if the request fails
-  }
+  // Parse the JSON response
+  const todos = JSON.parse(body);
 
-  // Iterate through the fetched data to count completed tasks per user
-  for (const task of data) {
-    if (task.completed) {
-      // If the task is completed, increment the user's count or initialize to 1
-      completedTasksByUser[task.userId] = (completedTasksByUser[task.userId] || 0) + 1;
+  // Create a dictionary to store the number of completed tasks for each user
+  const completedTasks = {};
+
+  // Iterate over the todos
+  todos.forEach(todo => {
+    // Get the user ID
+    const userId = todo.userId;
+
+    // If the task is completed, increment the counter for the user
+    if (todo.completed) {
+      if (!completedTasks.hasOwnProperty(userId)) {
+        completedTasks[userId] = 0;
+      }
+      completedTasks[userId]++;
+    }
+  });
+
+  // Print the users with completed tasks
+  for (const userId in completedTasks) {
+    if (completedTasks[userId] > 0) {
+      console.log(`${userId}: ${completedTasks[userId]}`);
     }
   }
-
-  // Print the results
-  console.log(completedTasksByUser);
 });
